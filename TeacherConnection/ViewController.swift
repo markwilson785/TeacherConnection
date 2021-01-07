b9a278410dbd600de62db59c3d64cf968e30a01e
mark <mark.wilson7203@stu.mtec.edu>
2021-01-07 08:29:16 -0700

    1-7-21
    color change. randomize working. apple logo button

--- TeacherConnection/ViewController.swift
+++ TeacherConnection/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var teachers: [String] = []
+    var students: [String] = []
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBOutlet weak var teacherLabel: UILabel!
+    
+    enum TableSection: Int {
+        case teachers
+        case students
     }
-
-
+    
+    @IBAction func RandomButton(_ sender: Any){
+        guard let randomTeachers = teachers.randomElement(),
+              let randomStudents = students.randomElement() else { return }
+        teacherLabel.text = String(randomTeachers)
+        displayLabel.text = String(randomStudents)
+        
+    }
+    
+    override func viewDidLoad() {
+        teachers = ["Parker","Ben","Johnny","Kevin","Jeff"]
+        students = ["Mark","Michael","Melissa","Brianna","Talmage"]
+    }
+    
+    
 }
 
